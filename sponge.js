34e5cc535880b6e4e4151dce72f4bb3581ea5f69
ktf <not.discostoo@gmail.com>
2012-09-20 21:39:37 -0700

    create Sponge object

--- sponge.js
+++ sponge.js
@@ -4,6 +4,25 @@ var SCREEN_HEIGHT = 600;
 var canvas;
 var context;
 
+var sponge; // the player
+
+
+function Sponge() {
+  this.centerX = canvas.width / 2;
+  this.centerY = canvas.height / 2;
+  this.radius = 70;
+
+  this.draw = function() {
+    context.beginPath();
+    context.arc(this.centerX, this.centerY, this.radius, 0, 2 * Math.PI, false);
+    context.fillStyle = 'green';
+    context.fill();
+    context.lineWidth = 5;
+    context.strokeStyle = '#003300';
+    context.stroke();
+  }
+}
+
 
 function init() {
   canvas = document.getElementById('world');
@@ -22,6 +41,8 @@ function init() {
   canvas.style.left = (window.innerWidth - SCREEN_WIDTH) * 0.5 + 'px';
   canvas.style.top = (window.innerHeight - SCREEN_HEIGHT) * 0.5 + 'px';
 
+  sponge = new Sponge()
+
   setInterval(loop, 1000 / 60);
 }
 
@@ -30,17 +51,7 @@ function loop() {
   context.fillStyle = 'rgba(0,0,0,0.05)';
   context.fillRect(0, 0, context.canvas.width, context.canvas.height);
 
-  var centerX = canvas.width / 2;
-  var centerY = canvas.height / 2;
-  var radius = 70;
-
-  context.beginPath();
-  context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-  context.fillStyle = 'green';
-  context.fill();
-  context.lineWidth = 5;
-  context.strokeStyle = '#003300';
-  context.stroke();
+  sponge.draw()
 }
 
 
